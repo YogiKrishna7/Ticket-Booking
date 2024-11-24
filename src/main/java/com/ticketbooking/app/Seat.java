f67d2331fab31cf2c5f254460e558a13844de5aa
Yogendra <a.yogendra65@gmail.com>
2024-11-25 03:44:16 +0530

    Updated Ticket Booking System

---
+++ src/main/java/com/ticketbooking/app/Seat.java
@@ -0,0 +1,40 @@
+package com.ticketbooking.app;
+
+public class Seat {
+    int seatId;
+    int screenId;
+    int seatNumber;
+    int price;
+
+    public int getSeatId() {
+        return seatId;
+    }
+
+    public void setSeatId(int seatId) {
+        this.seatId = seatId;
+    }
+
+    public int getScreenId() {
+        return screenId;
+    }
+
+    public void setScreenId(int screenId) {
+        this.screenId = screenId;
+    }
+
+    public int getSeatNumber() {
+        return seatNumber;
+    }
+
+    public void setSeatNumber(int seatNumber) {
+        this.seatNumber = seatNumber;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
